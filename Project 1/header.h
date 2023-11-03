909736e704323a269c67c334f54b4792a5b4c4f7
xavierjeffy <xjeffy22@gmail.com>
2023-11-03 16:26:31 -0400

    Header File Added

--- Project 1/header.h
+++ Project 1/header.h
@@ -9,14 +9,13 @@
 #define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); \
                     } while (0)
 
-#define buffSize 2   /* Maximum size for exchanged string */
+#define buffSize 2   // Maximum size for exchanged string
 
-/* Define a structure that will be imposed on the shared
-memory object */
+// Define a structure that will be imposed on the shared memory object
 
 struct sharedMem {
-    sem_t  sem1;            /* POSIX unnamed semaphore */
-    sem_t  sem2;            /* POSIX unnamed semaphore */
-    size_t cnt;             /* Number of bytes used in 'buf' */
-    char   buf[buffSize];   /* Data being transferred */
+    sem_t  sem1;            // unnamed semaphore
+    sem_t  sem2;            // unnamed semaphore
+    size_t cnt;             // Number of bytes used in 'buf'
+    char   buf[buffSize];   // Data being transferred
 };
