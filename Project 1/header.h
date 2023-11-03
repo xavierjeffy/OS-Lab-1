ae25dc44dc20f1fd2b66ce00e6eabe92be6943c4
Xavier Jeffy <xaiverjeffy@2019-Macbook-Pro.local>
2023-11-03 16:15:55 -0400

    Header file added

---
+++ Project 1/header.h
@@ -0,0 +1,22 @@
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); \
+                    } while (0)
+
+#define buffSize 2   /* Maximum size for exchanged string */
+
+/* Define a structure that will be imposed on the shared
+memory object */
+
+struct sharedMem {
+    sem_t  sem1;            /* POSIX unnamed semaphore */
+    sem_t  sem2;            /* POSIX unnamed semaphore */
+    size_t cnt;             /* Number of bytes used in 'buf' */
+    char   buf[buffSize];   /* Data being transferred */
+};
